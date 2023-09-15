1af18f116bcad8709e5c3b69e0515ccd3285d4d7
Alexander Mikhalev <alex@metacortex.engineer>
2023-09-15 13:47:18 +0100

    WIP

--- crates/terraphim_pipeline/src/lib.rs
+++ crates/terraphim_pipeline/src/lib.rs
@@ -1,7 +1,114 @@
-use ahash::AHashMap;
+use ahash::{AHashMap, HashMap};
 use memoize::memoize;
 use regex::Regex;
+use std::collections::hash_map::Entry;
+use std::collections::BTreeMap;
+use std::mem;
 use unicode_segmentation::UnicodeSegmentation;
+
+#[derive(Debug, Clone)]
+pub struct RoleGraph {
+    // role filter
+    role: String,
+    nodes: AHashMap<u64, Node>,
+    edges: AHashMap<u64, Edge>,
+    automata_url: String,
+}
+impl RoleGraph {
+    pub fn new(role: String, automata_url: String) -> Self {
+        Self {
+            role,
+            nodes: AHashMap::new(),
+            edges: AHashMap::new(),
+            automata_url: automata_url,
+        }
+    }
+    pub fn query(&self) {
+        for (node_id, node) in &self.nodes {
+            println!("{:?}", node);
+            node.sort_edges_by_value();
+        }
+    }
+    pub fn add_or_update_article(&mut self, article_id: String, x: u64, y: u64) {
+        let edge = magic_pair(x, y);
+        let edge = self.init_or_update_edge(edge, article_id);
+        self.init_or_update_node(x, &edge);
+        self.init_or_update_node(y, &edge);
+    }
+    fn init_or_update_node(&mut self, node_id: u64, edge: &Edge) {
+        match self.nodes.entry(node_id) {
+            Entry::Vacant(_) => {
+                let node = Node::new(node_id, edge.clone());
+                self.nodes.insert(node.id, node);
+            }
+            Entry::Occupied(entry) => {
+                let mut node = entry.into_mut();
+                node.rank += 1;
+            }
+        };
+    }
+    fn init_or_update_edge(&mut self, edge_key: u64, article_id: String) -> Edge {
+        let edge = match self.edges.entry(edge_key) {
+            Entry::Vacant(_) => {
+                let edge = Edge::new(edge_key, article_id);
+                self.edges.insert(edge.id, edge.clone());
+                edge
+            }
+            Entry::Occupied(entry) => {
+                let mut edge = entry.into_mut();
+                *edge.doc_hash.entry(article_id).or_insert(1) += 1;
+                let edge_read = edge.clone();
+                edge_read
+            }
+        };
+        edge
+    }
+}
+#[derive(Debug, Clone)]
+pub struct Edge {
+    // id of the node
+    id: u64,
+    rank: u64,
+    // hashmap document_id, rank
+    doc_hash: AHashMap<String, u32>,
+}
+impl Edge {
+    pub fn new(id: u64, article_id: String) -> Self {
+        let mut doc_hash = AHashMap::new();
+        doc_hash.insert(article_id, 1);
+        Self {
+            id,
+            rank: 1,
+            doc_hash: doc_hash,
+        }
+    }
+}
+// Node represent single concept
+#[derive(Debug, Clone)]
+pub struct Node {
+    id: u64,
+    // number of co-occureneces
+    rank: u64,
+    connected_with: AHashMap<u64, Edge>,
+}
+impl Node {
+    fn new(id: u64, edge: Edge) -> Self {
+        let mut connected_with = AHashMap::new();
+        connected_with.insert(edge.id, edge);
+        Self {
+            id,
+            rank: 1,
+            connected_with: connected_with,
+        }
+    }
+    pub fn sort_edges_by_value(&self) {
+        // let count_b: BTreeMap<&u64, &Edge> =
+        // self.connected_with.iter().map(|(k, v)| (v, k)).collect();
+        for (k, v) in self.connected_with.iter().map(|(k, v)| (v, k)) {
+            println!("k {:?} v {:?}", k, v);
+        }
+    }
+}
 
 #[macro_use]
 extern crate lazy_static;
--- crates/terraphim_pipeline/src/main.rs
+++ crates/terraphim_pipeline/src/main.rs
@@ -2,8 +2,8 @@ use itertools::Itertools;
 use terraphim_automata::load_automata;
 use terraphim_automata::matcher::{find_matches, find_matches_ids, replace_matches, Dictionary};
 use terraphim_pipeline::split_paragraphs;
-use terraphim_pipeline::{magic_pair, magic_unpair};
-
+use terraphim_pipeline::{magic_pair, magic_unpair, RoleGraph};
+use ulid::Ulid;
 fn main() {
     let paragraph = "This is the first sentence.\n\n This is the second sentence. This is the second sentence? This is the second sentence| This is the second sentence!\n\nThis is the third sentence. Mr. John Johnson Jr. was born in the U.S.A but earned his Ph.D. in Israel before joining Nike Inc. as an engineer. He also worked at craigslist.org as a business analyst.";
     println!("Sentence segmentation test");
@@ -11,9 +11,10 @@ fn main() {
         println!("Sentence {}", sentence);
     }
     println!("System operator role");
+    let role = "system operator".to_string();
     let automata_url = "https://system-operator.s3.eu-west-2.amazonaws.com/term_to_id.json";
     let dict_hash = load_automata(automata_url).unwrap();
-    let query = "I am a text with the word Life cycle concepts and bar and Trained operators and maintainers";
+    let query = "I am a text with the word Life cycle concepts and bar and Trained operators and maintainers, project direction, some bingo words Paradigm Map and project planning, then again: some bingo words Paradigm Map and project planning, then repeats: Trained operators and maintainers, project direction";
     let matches = find_matches(query, dict_hash.clone(), false);
     println!("Matches: {:?}", matches);
     let matches2 = replace_matches(query, dict_hash.clone()).unwrap();
@@ -22,11 +23,24 @@ fn main() {
     let matches3 = find_matches_ids(query, &dict_hash).unwrap();
     println!("Matched Ids {:?}", matches3);
     let mut v = Vec::new();
+    let mut rolegraph = RoleGraph::new(role, automata_url.to_string());
+    let article_id = Ulid::new().to_string();
     for (a, b) in matches3.into_iter().tuple_windows() {
+        println!("a {} b {}", a, b);
+        rolegraph.add_or_update_article(article_id.clone(), a, b);
         v.push(magic_pair(a, b));
     }
-    println!("Magic Pair {:?}", v);
+    println!("Magic Pairs {:?}", v);
+
+    println!("Magic unpar");
     for z in v.into_iter() {
         println!("{:?}", magic_unpair(z));
     }
+    println!("{:?}", rolegraph);
+    println!("Query graph");
+    // TODO: now query edges given nodes, shall query will be cantor product?
+    // query: matched query to nodes
+    // for each node return list of all edges (in this node)
+    // sort by node rank, edge_rank, article_id rank
+    rolegraph.query();
 }
